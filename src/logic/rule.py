bf65f29682d242f6032fe067aa359b30524b1dce
Sumaiyah Kola <sumaiyahkola1@gmail.com>
2019-12-30 19:31:30 +0000

    Alter Rule Representation to DNF Clausal form

--- src/logic/rule.py
+++ src/logic/rule.py
@@ -1,7 +1,10 @@
 from enum import Enum
 from typing import Union, Set, List
 
-from NN_model.neural_network_model import Classifiation
+from logic.clause import DisjunctiveClause, ConjunctiveClause, Clause
+from logic.normal_form import DNF
+from logic.literal import Literal
+
 
 class Neuron:
     """
@@ -27,6 +30,9 @@ class Neuron:
     def __hash__(self):
         return hash(str(self))
 
+    def get_index(self):
+        return self.index
+
 class TermOperator(Enum):
     GreaterThan = '>'
     LessThanEq = '<='
@@ -43,14 +49,14 @@ class TermOperator(Enum):
         if self.LessThanEq:
             return self.GreaterThan
 
-class Term:
+class Term(Literal):
     """
     Represent a term in a Rule or a condition on a branch in a RuleTree e.g. (6 > 7), (Neuron(1,2) > 0.9)
     """
 
     __slots__ = ['neuron', 'operator', 'operand']
 
-    def __init__(self, neuron: Neuron, operator: str, operand):
+    def __init__(self, neuron: Neuron, operator: str, operand: float):
         self.neuron = neuron
         self.operand = operand
 
@@ -80,7 +86,7 @@ class Term:
         elif self.operator is TermOperator.LessThanEq:
             return value <= self.operand
 
-    def inverse(self):
+    def negate(self):
         """
         Return inverse of the term i.e. the same term with opposite sign
         """
@@ -90,23 +96,48 @@ class Term:
         """
         return neuron index of term neuron is applied to
         """
-        return self.neuron.index
+        return self.neuron.get_index()
+
+class Classifiation(Literal):
+    """
+    Represent classification of instance with index used when encoding instanc in network
+    """
+
+    __slots__ = ['class_name', 'class_index']
+
+    def __init__(self, class_name: str, class_index: int):
+        self.class_name = class_name
+        self.class_index = class_index
+
+    def __str__(self):
+        return 'OUT=' + self.class_name
+
+    def __eq__(self, other):
+        return (
+            self.__class__ == other.__class__ and
+            self.class_name == other.class_name
+        )
+
+    def __hash__(self):
+        return hash(str(self))
+
+    def negate(self):
+        print('implement this!')
 
 class Rule:
     """
-    Represent if-then rule (path from root to leaf in RuleTree with premise of ANDed Terms and Conclusion
+    Represent if-then rule as an object with DNF premise and Term/Classification conclusion
     """
 
     __slots__ = ['premise', 'conclusion']
 
-    def __init__(self, premise: Set[Term], conclusion: Union[Term, Classifiation]):
+    def __init__(self, premise: DNF(clauses=Set[ConjunctiveClause]), conclusion: Union[Term, Classifiation]):
         self.premise = premise
         self.conclusion = conclusion
         # self.confidence = confidence TODO ADD/DO SOMETHING WITH CONDIFENCE
 
     def __str__(self):
-        conditions_str = [str(term) for term in self.premise]
-        return "IF " + (" AND ").join(conditions_str) + " THEN " + str(self.conclusion)
+        return "IF " + str(self.premise) + " THEN " + str(self.conclusion)
 
     def __eq__(self, other):
         return (
@@ -118,12 +149,11 @@ class Rule:
     def __hash__(self):
         return hash(str(self))
 
-    def get_terms(self) -> Set[Term]:
-        """
-        return terms in rule premise
-        """
+    def get_premise_clauses(self) -> Set[Clause]:
+        return self.premise.get_clauses()
+
+    def get_premise(self) -> DNF:
         return self.premise
 
     def get_conclusion(self) -> Union[Term, Classifiation]:
         return self.conclusion
-
