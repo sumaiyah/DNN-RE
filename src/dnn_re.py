7c36fc659f6c0661b9d62fa56cc3ca903a7b93bf
Sumaiyah Kola <sumaiyahkola1@gmail.com>
2020-03-28 17:07:10 +0000

    Refactored rule extraction code to find the best initialisation

---
+++ src/dnn_re.py
@@ -0,0 +1,64 @@
+"""
+Run 1 iteration of rule extraction
+"""
+import pandas as pd
+import numpy as np
+
+from evaluate_rules.evaluate import evaluate
+from evaluate_rules.predict import predict
+from model.model import Model
+from keras.models import load_model
+from model.generation import TEMP_DIR, DATASET_INFO, RULE_EX_MODE
+
+
+import memory_profiler
+from collections import namedtuple
+
+# Data is made up of X (input), y (target)
+DataValues = namedtuple('DataValues', 'X y')
+
+def run(X, y, train_index, test_index, model_file_path, label_file_path):
+    import time
+    # Split data
+    X_train, y_train = X[train_index], y[train_index]
+    X_test, y_test = X[test_index], y[test_index]
+
+    train_data = DataValues(X=X_train, y=y_train)
+    test_data = DataValues(X=X_test, y=y_test)
+
+    # Initialise NN Model object
+    NN_model = Model(model_path=model_file_path,
+                     output_classes=DATASET_INFO.output_classes,
+                     train_data=train_data,
+                     test_data=test_data,
+                     activations_path=TEMP_DIR + 'activations/')
+
+    # Save labels to labels.csv:
+    # true data labels
+    label_data = {'id': test_index,
+                  'true_labels': y_test}
+    # neural network data labels. Use NN to predict X_test
+    nn_model = load_model(model_file_path)
+    nn_predictions = np.argmax(nn_model.predict(X_test), axis=1)
+    label_data['nn_labels'] = nn_predictions
+
+    # Rule Extraction
+    start_time, start_memory = time.time(), memory_profiler.memory_usage()[0]
+    rules = RULE_EX_MODE.run(NN_model)
+    end_time, end_memory = time.time(), memory_profiler.memory_usage()[0]
+
+    # Use rules for prediction
+    NN_model.set_rules(rules)
+    rule_predictions = predict(rules, test_data.X)
+    label_data['rule_%s_labels' % RULE_EX_MODE.mode] = rule_predictions
+    pd.DataFrame(data=label_data).to_csv(label_file_path, index=False)
+
+    time = end_time - start_time
+    memory = end_memory - start_memory
+
+    # Evaluation
+    results = evaluate(rules, label_file_path)
+    results['time'] = time
+    results['memory'] = memory
+
+    return results
---
+++ src/model/generation/helpers/generate_data.py
@@ -0,0 +1,53 @@
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+
+from model.generation import RESULTS_DIR
+from model.generation.helpers.build_and_train_model import create_model
+
+
+def grid_search(X, y):
+    """
+
+    Args:
+        X: input features
+        y: target
+
+    Returns:
+        batch_size: best batch size
+        epochs: best number of epochs
+        layer_1: best number of neurons for layer 1 (first hidden layer)
+        layer_2: best number of neurons for layer 2
+
+    Perform a 5-folded grid search over the neural network hyper-parameters
+    """
+    batch_size = [10, 20, 50, 100]
+    epochs = [50, 100, 200, 400]
+    layer_1 = [10, 15, 50, 100]
+    layer_2 = [2, 5, 10, 50]
+
+    param_grid = dict(batch_size=batch_size,
+                      epochs=epochs,
+                      layer_1=layer_1,
+                      layer_2=layer_2)
+
+    model = KerasClassifier(build_fn=create_model, verbose=0)
+
+    grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1, cv=5)
+    grid_result = grid.fit(X, y)
+
+    # Write best results to file
+    grid_search_results_file_path = RESULTS_DIR + 'grid_search_results.txt'
+    with open(grid_search_results_file_path, 'w') as file:
+        file.write("Best: %f using %s \n" % (grid_result.best_score_, grid_result.best_params_))
+
+        means = grid_result.cv_results_['mean_test_score']
+        stds = grid_result.cv_results_['std_test_score']
+        params = grid_result.cv_results_['params']
+        for mean, stdev, param in zip(means, stds, params):
+            file.write("%f (%f) with: %r\n" % (mean, stdev, param))
+
+    print('Grid Search for hyper parameters complete.')
+    print("Best: %f using %s \n" % (grid_result.best_score_, grid_result.best_params_))
+
+    return grid_result.best_params_['batch_size'], grid_result.best_params_['epochs'], \
+           grid_result.best_params_['layer_1'], grid_result.best_params_['layer_2']
